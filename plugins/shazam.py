190f8adbe591540e55b05209dd2b45e573a4b2ac
Kei <73867910+KeinShin@users.noreply.github.com>
2021-06-18 18:49:53 +0530

    Fix exception

--- plugins/shazam.py
+++ plugins/shazam.py
@@ -9,7 +9,7 @@
 import os
 
 import requests
-
+from json import JSONDecodeError
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, fetch_audio
 
